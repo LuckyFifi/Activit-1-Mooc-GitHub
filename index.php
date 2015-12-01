1301b880e71a956417ea8282d9de7b9d14a6604a
Filip Ravel <warmup95@hotmail.fr>
2015-12-01 17:44:06 +0100

    ajout du fichier CSS

--- index.php
+++ index.php
@@ -2,14 +2,17 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
 		<title>Accueil</title>
 	</head>
 	<body>
-		<header>
-			<h1>Bienvenue sur ce site !</h1>
-		</header>	
-		<section>
-			<p>Hello world</p>
-		</section>
+		<div id="bloc_page">
+			<header>
+				<h1>Bienvenue sur ce site !</h1>
+			</header>	
+			<section>
+				<p>Hello world</p>
+			</section>
+		</div>
 	</body>
 </html>
