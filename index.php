6f1979fb0669e4d7293da674e900deb88686d1b3
Filip Ravel <warmup95@hotmail.fr>
2015-12-01 16:59:01 +0100

    commit initial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Accueil</title>
+	</head>
+	<body>
+		<header>
+			<h1>Bienvenue sur ce site !</h1>
+		</header>	
+		<section>
+			<p>Hello world</p>
+		</section>
+	</body>
+</html>
